3275b32e52cfb690e2bdd4214c401dcfcbc1e2ba
Kenford <kzhou0066@gmail.com>
2019-05-06 01:42:55 -0400

    added calculator command

--- tagger-bot.js
+++ tagger-bot.js
@@ -22,14 +22,14 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 
 bot.onText(/(^\/taskete$)|(^\/h(e|a)lp$)/, async(msg, match) => {
-  
   bot.sendMessage(msg.chat.id, `
-/roll  |      rolls a die by default, 
-/roll #  |   gives a random number up to the number you input
-/meena  |   tags everyone in the group
-/flip  |       flips a coin
+/roll = rolls a die by default, 
+/roll # = gives a random number up to the number you input
+/meena = tags everyone in the group
+/flip = flips a coin
+/calc # (op) # = calculator
   `);
-})
+});
 
 // tags everyone in my group
 bot.onText(/^\/meena$/, async(msg, match) => {
@@ -37,7 +37,7 @@ bot.onText(/^\/meena$/, async(msg, match) => {
     //const member = await bot.getChatMember(chatId, user);
     //console.log(member);
     bot.sendMessage(msg.chat.id, process.env.TELEGRAM_GROUP_USERS);
-})
+  });
 
 // rolls a die
 bot.onText(/^\/roll( [0-9]*)?$/, (msg, match) => {
@@ -48,10 +48,29 @@ bot.onText(/^\/roll( [0-9]*)?$/, (msg, match) => {
   }
 
   bot.sendMessage(msg.chat.id, `Random Number: ${Math.floor(Math.random() * threshold) + 1}!`);
-})
+});
 
 bot.onText(/(\/flip)/, (msg, match) => {
   let coin = Math.round(Math.random());
   console.log(coin)
   bot.sendMessage(msg.chat.id, coin === 0 ? 'Heads' : 'Tails');
-})
+});
+
+bot.onText(/^\/calc \d+ ?(\+|\-|\*|\/){1} ?\d+$/, (msg, match) => {
+  let operand1 = parseInt(match[0].split(' ')[1]);
+  let operand2 = parseInt(match[0].split(' ')[3]);
+  let operation = match[0].split(' ')[2];
+  let result;
+  console.log(match);
+  console.log(match[0]);
+
+  switch(operation) {
+    case '+': result = operand1 + operand2; break;
+    case '-': result = oprand1 - operand2; break;
+    case '*': result = operand1 * operand2; break;
+    case '/': result = operand1 / operand2; break;
+    default: result = 'Something is wrong with your input! Be sure to add spaces between numbers.'; break;
+  }
+
+  bot.sendMessage(msg.chat.id, `Result: ${result}`);
+});
