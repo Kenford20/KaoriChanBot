b42e78eab1f6e9ac665a2974d8f5613d075fc2a8
Kenford <kzhou0066@gmail.com>
2019-05-06 02:36:52 -0400

    added calculation command to help command list

--- tagger-bot.js
+++ tagger-bot.js
@@ -29,6 +29,7 @@ bot.onText(/(^\/taskete$)|(^\/h(e|a)lp$)/, async(msg, match) => {
 /meena = tags everyone in the group
 /flip = flips a coin
 /calc # (op) # = calculator
+/convert (unit) to (unit) = general units conversion
   `);
 });
 
@@ -51,18 +52,13 @@ bot.onText(/^\/roll( [0-9]*)?$/, (msg, match) => {
   bot.sendMessage(msg.chat.id, `Random Number: ${Math.floor(Math.random() * threshold) + 1}!`);
 });
 
-bot.onText(/(\/flip)/, (msg, match) => {
+// flips a coin
+bot.onText(/^\/flip$/, (msg, match) => {
   let coin = Math.round(Math.random());
   bot.sendMessage(msg.chat.id, coin === 0 ? 'Heads' : 'Tails');
 });
 
-// bot.onText(/^\/calc \d+ ?(\+|\-|\*|\/){1} ?\d+$/, (msg, match) => {
-//   let result = math.eval(match[0].slice(5));
-//   console.log(result);
-
-//   bot.sendMessage(msg.chat.id, `Result: ${result}`);
-// });
-
+// uses mathjs library to do mathematical calculations and unit conversions
 bot.onText(/^\/(calc|convert) .+$/, async(msg, match) => {
   console.log(match[0]);
   try {
@@ -72,4 +68,8 @@ bot.onText(/^\/(calc|convert) .+$/, async(msg, match) => {
     console.log(err);
     bot.sendMessage(msg.chat.id, `Something went wrong. Double check your inputs bro!`);
   }
+});
+
+bot.onText(/weeb/i, (msg, match) => {
+  bot.sendMessage(msg.chat.id, `Y-y.. yes... sen..p-pai..?`);
 });
