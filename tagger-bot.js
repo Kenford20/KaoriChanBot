421f9eea58ef831b8652826f2274fe917a53fa42
Kenford <kzhou0066@gmail.com>
2019-05-08 05:28:10 -0400

    added emoji logic to weather command and updated list of commands

--- tagger-bot.js
+++ tagger-bot.js
@@ -32,6 +32,7 @@ bot.onText(/(^\/taskete$)|(^\/h(e|a)lp$)/, async(msg, match) => {
 /flip = flips a coin
 /calc # (op) # = calculator
 /convert (unit) to (unit) = general units conversion
+/weather (city) = gives you the weather in the city you specify
 and some weeb stuff
   `);
 });
@@ -58,7 +59,10 @@ bot.onText(/^\/roll( [0-9]*)?$/, (msg, match) => {
 // flips a coin
 bot.onText(/^\/flip$/, (msg, match) => {
   let coin = Math.round(Math.random());
-  bot.sendMessage(msg.chat.id, coin === 0 ? 'Heads' : 'Tails');
+  let smiley = '\u{1F604}';
+  let peach = '\u{1F351}';
+
+  bot.sendMessage(msg.chat.id, coin === 0 ? `Heads ${smiley}` : `Tails ${peach}`);
 });
 
 // uses mathjs library to do mathematical calculations and unit conversions
@@ -76,25 +80,54 @@ bot.onText(/^\/(calc|convert) .+$/, async(msg, match) => {
 bot.onText(/^\/weather .+$/i, async(msg, match) => {
   const fetch = require('node-fetch');
   const city = match[0].slice(match[0].indexOf(' '));
-
-  if(city.includes(' ')) {
-    city.replace(/\s/g, '+');
-  }
-  console.log(city);
-
   const weatherAPI = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.WEATHER_API_KEY}&units=imperial`; // units=imperial converts temperature to Fahrenheit
 
+  if(city.includes(' ')) {
+    city.replace(/\s/g, '+'); // handles queries for cities with spaces (i.e san franscisco)
+  }
+
+  // # Openweathermap Weather codes and corresponding emojis
+  const emojis = {
+    thunderstorm: '\u{1F4A8}',    // # Code: 200's, thunderstorms
+    droplet: '\u{1F4A7}',         // # Code: 300's drizzle
+    rain: '\u{02614}',            // # Code: 500's rain
+    snowflake: '\u{02744}',       // # Code: 600's snow
+    snowman: '\u{026C4}',         // # Code: 600's snowman
+    atmosphere: '\u{1F301}',      // # Code: 700's atmosphere (mist, smoke, haze, fog, dust, sand)
+    sun: '\u{02600}',             // # Code: 800 clear sky
+    sunCloud: '\u{026C5}',        // # Code: 801/802 partly cloudly (11-50%)
+    cloud: '\u{02601}',           // # Code: 803/804 cloudy (50-100%)
+    fire: '\u{1F525}',            // # Code: 904
+    defaultEmoji: '\u{1F300}'     // # default emojis
+  };
+
   try {
-    let response = await fetch(weatherAPI, {
+    const response = await fetch(weatherAPI, {
       method: "POST",
       header: { "Content-Type": "application/json" }
     });
-    let data = await response.json();
-    console.log((data));
+    const data = await response.json();
+    const weatherCode = data.weather[0].id;
+    const temperatureEmoji = data.main.temp > 50 ? emojis.fire : emojis.snowman;
+    let weatherEmoji;
+
+    // weather codes at: https://openweathermap.org/weather-conditions
+    switch(true) {
+      case (weatherCode >= 200 && weatherCode <= 232): weatherEmoji = emojis.thunderstorm; break;
+      case (weatherCode >= 300 && weatherCode <= 321): weatherEmoji = emojis.droplet; break;
+      case (weatherCode >= 500 && weatherCode <= 531): weatherEmoji = emojis.rain; break;
+      case (weatherCode >= 600 && weatherCode <= 622): weatherEmoji = emojis.snowflake; break;
+      case (weatherCode >= 700 && weatherCode <= 781): weatherEmoji = emojis.atmosphere; break;
+      case weatherCode === 800: weatherEmoji = emojis.sun; break;
+      case (weatherCode === 801 || weatherCode === 802): weatherEmoji = emojis.sunCloud; break;
+      case (weatherCode === 803 || weatherCode === 804): weatherEmoji = emojis.cloud; break;
+      default: weatherEmoji = emojis.defaultEmoji;
+    }
+
     bot.sendMessage(msg.chat.id, `
-      Current weather in ${data.name}, ${data.sys.country}: \n
-      Temp is ${data.main.temp}${String.fromCharCode(176)}F, (${data.main.temp_max}${String.fromCharCode(176)}F high and ${data.main.temp_min}${String.fromCharCode(176)}F low)\n
-      Forecast is ${data.weather[0].main} and ${data.weather[0].description}
+      Current weather in ${data.name},  (${data.sys.country}): \n
+      ${temperatureEmoji} Temp is ${data.main.temp}${String.fromCharCode(176)}F, (${data.main.temp_max}${String.fromCharCode(176)}F high and ${data.main.temp_min}${String.fromCharCode(176)}F low)\n
+      ${weatherEmoji} Forecast is ${data.weather[0].main} and ${data.weather[0].description}
     `);
   } catch(err) {
       console.log(err);
