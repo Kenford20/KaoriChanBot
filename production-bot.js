efc7211d146eb822e525281362f747544184dc72
Kenford <kzhou0066@gmail.com>
2019-05-14 22:44:04 -0400

    fixed bug with spotify responses posting to wrong chat id

--- production-bot.js
+++ production-bot.js
@@ -253,8 +253,8 @@ bot.on('callback_query', async(callbackQuery) => {
   const queryType = callbackQuery.data.slice(0, callbackQuery.data.indexOf(' '));
   const queryInput = callbackQuery.data.slice(callbackQuery.data.indexOf(' ') + 1);
 
-  bot.deleteMessage(callbackQuery.from.id, callbackQuery.message.message_id);
-  bot.sendMessage(callbackQuery.from.id, `You queried for: ${queryType}`);
+  bot.deleteMessage(callbackQuery.message.chat.id, callbackQuery.message.message_id);
+  bot.sendMessage(callbackQuery.message.chat.id, `You queried for: ${queryType}`);
   
   const spotifyAPI = `https://api.spotify.com/v1/search?q=${(queryInput).toLowerCase()}&type=${(queryType).toLowerCase()}&limit=5&access_token=${accessToken}`
 
@@ -292,17 +292,17 @@ bot.on('callback_query', async(callbackQuery) => {
           accessToken = response.data.access_token;
         }).catch(tokenErr => {
           console.log(`axios err: ${tokenErr}`);
-          bot.sendMessage(callbackQuery.from.id, 'Failed to generate a new access token for some reason, look into it Kenford!!');
+          bot.sendMessage(callbackQuery.message.chat.id, 'Failed to generate a new access token for some reason, look into it Kenford!!');
         });
       } else {
         errResponseMsg = 'Failed to fetch song data, double check your query!';
       }
-      bot.sendMessage(callbackQuery.from.id, errResponseMsg);
+      bot.sendMessage(callbackQuery.message.chat.id, errResponseMsg);
     } 
     else {
       switch(queryType.replace(/\s/g, '')) {
         case 'Track': {
-          bot.sendMessage(callbackQuery.from.id, `
+          bot.sendMessage(callbackQuery.message.chat.id, `
 Here's the track: ${spotifyData.tracks.items[0].external_urls.spotify} 
 Track album: ${spotifyData.tracks.items[0].album.name}, which has ${spotifyData.tracks.items[0].album.total_tracks} tracks
 Album release: ${spotifyData.tracks.items[0].album.release_date}
@@ -310,28 +310,28 @@ If you don't have a spotify account, here's a preview fam dw: ${spotifyData.trac
           `);
         } break;
         case 'Album': {
-          bot.sendMessage(callbackQuery.from.id, `
+          bot.sendMessage(callbackQuery.message.chat.id, `
 Here's the album: ${spotifyData.albums.items[0].external_urls.spotify} 
 Album is called ${spotifyData.albums.items[0].name} has ${spotifyData.albums.items[0].total_tracks} tracks.
 Album release: ${spotifyData.albums.items[0].release_date}
           `);
         } break;
         case 'Artist': {
-          bot.sendMessage(callbackQuery.from.id, `
+          bot.sendMessage(callbackQuery.message.chat.id, `
 Here's a profile: ${spotifyData.artists.items[0].external_urls.spotify} 
 ${spotifyData.artists.items[0].name} has ${spotifyData.artists.items[0].followers.total} followers! 
 Artist's genres: ${spotifyData.artists.items[0].genres.join(' ')} 
           `);
         } break;
         case 'Playlist': {
-          bot.sendMessage(callbackQuery.from.id, `
+          bot.sendMessage(callbackQuery.message.chat.id, `
 Here's the playlist: ${spotifyData.playlists.items[0].external_urls.spotify} 
 Playlist is called: ${spotifyData.playlists.items[0].name}
 Created by: ${spotifyData.playlists.items[0].owner.display_name}
 You can view more of his/her stuff at: ${spotifyData.playlists.items[0].owner.external_urls.spotify}
           `);
         } break;
-        default: bot.sendMessage(callbackQuery.from.id, 'Something went wrong, try again oof'); break;
+        default: bot.sendMessage(callbackQuery.message.chat.id, 'Something went wrong, try again oof'); break;
       }
     }
   } catch(fetchErr) {
